8aaaf4dfc9a92a9f8d451511e1a434109bd7c270
kaktusbezcherepa <eli1starboom@gmail.com>
2024-10-12 20:43:20 +0300

    initital

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import '../../css/app.css'
+
+
+export default function Welcome() {
+    return (
+        <div>
+            <h1>заработало</h1>
+            
+        </div>
+    );
+}
