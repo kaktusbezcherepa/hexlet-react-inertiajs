fe00a6e2d7d0da8bffe47ed7413319c7831b40b9
kaktusbezcherepa <eli1starboom@gmail.com>
2024-10-14 01:02:36 +0300

    preFrom

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,12 +1,84 @@
 import React from 'react';
-// import '../../css/app.css'
-
+import { Button, Col, Form, Container, Row } from 'react-bootstrap';
 
 export default function Welcome() {
     return (
-        <div>
-            <h1>заработало</h1>
-            
-        </div>
+        <Container fluid className="d-flex justify-content-center align-items-center vh-100">
+            <Form noValidate className="w-50">
+                <Row>
+                    <Col md="6">
+                        <Form.Group controlId="validationCustom01">
+                            <Form.Label>Имя</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Вася"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                    </Col>
+                    <Col md="6">
+                        <Form.Group controlId="validationCustom02">
+                            <Form.Label>Фамилия</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Пупкин"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md="6">
+                        <Form.Group controlId="validationCustom03">
+                            <Form.Label>Дата рождения</Form.Label>
+                            <Form.Control type="date" required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid date.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Col>
+                    <Col md="6">
+                        <Form.Group controlId="validationCustom04">
+                            <Form.Label>Пол</Form.Label>
+                            <Form.Check
+                                type="radio"
+                                label="Мужской"
+                                name="gender"
+                                id="genderMale"
+                                required
+                            />
+                            <Form.Check
+                                type="radio"
+                                label="Женский"
+                                name="gender"
+                                id="genderFemale"
+                                required
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                Please select your gender.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3">
+                            <Form.Check
+                                required
+                                label="Agree to terms and conditions"
+                                feedback="You must agree before submitting."
+                                feedbackType="invalid"
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Button type="submit">Submit form</Button>
+            </Form>
+        </Container>
     );
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>hexlet-test</title>
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
